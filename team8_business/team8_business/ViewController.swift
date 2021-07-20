4e62c125852a9bc696e90a3fdc948a551d482463
Phillips <phizzarfernos@gmail.com>
2021-07-20 13:44:47 +0700

    init

---
+++ team8_business/team8_business/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  team8_business
+//
+//  Created by Vincentius Phillips Zhuputra on 20/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ team8_business/team8_businessTests/team8_businessTests.swift
@@ -0,0 +1,33 @@
+//
+//  team8_businessTests.swift
+//  team8_businessTests
+//
+//  Created by Vincentius Phillips Zhuputra on 20/07/21.
+//
+
+import XCTest
+@testable import team8_business
+
+class team8_businessTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
