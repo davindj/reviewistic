dc8cbfc6f7087a333e590e718fbac926c1ea583a
Davin DJ <davindx88@gmail.com>
2022-08-17 21:49:48 +0700

    Tab Bar - Fix Invis iOS 15 Tab bar issues

--- team8_business/team8_business/Shared/SceneDelegate.swift
+++ team8_business/team8_business/Shared/SceneDelegate.swift
@@ -16,6 +16,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if #available(iOS 13.0, *) {
+            let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithDefaultBackground()
+            if #available(iOS 15.0, *) {
+                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+           }
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
